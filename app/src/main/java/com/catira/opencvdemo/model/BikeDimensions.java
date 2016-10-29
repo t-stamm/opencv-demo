911bdf611cdfd5288e83f408f8f9ee4bdbd7ecee
ck <ck@infomantis.de>
2016-10-29 11:29:07 +0200

    add bicycle variables

--- app/src/main/java/com/catira/opencvdemo/model/BikeDimensions.java
+++ app/src/main/java/com/catira/opencvdemo/model/BikeDimensions.java
@@ -6,4 +6,14 @@ package com.catira.opencvdemo.model;
 
 public class BikeDimensions {
 
+    double  bicycle_frame_size; /* Radstand */
+    double  bicycle_frame_size_ital; /* Rahmen ital. */
+    double  wheelbase; /* Radstand */
+    double  bicycle_trail; /* Nachlauf */
+    double  bending_recess; /* Gabelbiegung Rücksprung */
+    double  front_fork_ends; /* Gabelausfall Ende */
+    double  fork_head; /* Gabelkopf */
+    double  steering_head_length; /* Lenkkopflänge / - höhe */
+    double  back_drop_out; /* hinteres Ausfallende */
+
 }
