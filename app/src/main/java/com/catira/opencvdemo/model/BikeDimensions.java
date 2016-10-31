ec15b8f6d1e5c22d8d8eebb1d48c6eb1d427af29
t-stamm <timo.stamm@gmx.de>
2016-10-31 21:30:24 +0100

    coding guidelines + constructor and a few getters

--- app/src/main/java/com/catira/opencvdemo/model/BikeDimensions.java
+++ app/src/main/java/com/catira/opencvdemo/model/BikeDimensions.java
@@ -1,19 +1,59 @@
 package com.catira.opencvdemo.model;
 
+
 /**
  * Created by Timo on 26.10.2016.
  */
 
+import org.opencv.core.Point;
+
+/**
+ * Saves the bike key points for the wheels, saddle and steering.
+ * Other values are calculated
+ */
 public class BikeDimensions {
 
-    double  bicycle_frame_size; /* Radstand */
-    double  bicycle_frame_size_ital; /* Rahmen ital. */
-    double  wheelbase; /* Radstand */
-    double  bicycle_trail; /* Nachlauf */
-    double  bending_recess; /* Gabelbiegung Rücksprung */
-    double  front_fork_ends; /* Gabelausfall Ende */
-    double  fork_head; /* Gabelkopf */
-    double  steering_head_length; /* Lenkkopflänge / - höhe */
-    double  back_drop_out; /* hinteres Ausfallende */
 
+    protected Point mFrontWheel; /* Vorderrad Mittelpunkt */
+    protected Point mBackWheel; /* Hinterrad Mittelpunkt */
+    protected Point mForkHead; /* Gabelkopf */
+    protected Point mSaddle; /* Sattel */
+    protected Point mPaddles; /* Pedale */
+
+    // really necessary?
+    // Point  steering_head_length; /* Lenkkopflänge / - höhe */
+    // Point bicycleTrail; /* Nachlauf */
+    // Point bendingRecess; /* Gabelbiegung Rücksprung */
+
+    public BikeDimensions(Point mFrontWheel, Point mBackWheel, Point mForkHead, Point mSaddle, Point mPaddles) {
+        this.mFrontWheel = mFrontWheel;
+        this.mBackWheel = mBackWheel;
+        this.mForkHead = mForkHead;
+        this.mSaddle = mSaddle;
+        this.mPaddles = mPaddles;
+    }
+
+    public double getWheelbase() {
+        return Math.hypot(mFrontWheel.x, mBackWheel.x) + Math.hypot(mBackWheel.x, mBackWheel.y);
+    }
+
+    public Point getFrontWheel() {
+        return mFrontWheel;
+    }
+
+    public Point getBackWheel() {
+        return mBackWheel;
+    }
+
+    public Point getForkHead() {
+        return mForkHead;
+    }
+
+    public Point getSaddle() {
+        return mSaddle;
+    }
+
+    public Point getPaddles() {
+        return mPaddles;
+    }
 }
