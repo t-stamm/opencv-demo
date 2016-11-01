a9fbc8c69550674e23389434132ab3d978f6b7e8
ck <ck@infomantis.de>
2016-11-01 19:44:45 +0100

    add init personDimensions

--- app/src/main/java/com/catira/opencvdemo/model/PersonDimensions.java
+++ app/src/main/java/com/catira/opencvdemo/model/PersonDimensions.java
@@ -5,4 +5,26 @@ package com.catira.opencvdemo.model;
  */
 
 public class PersonDimensions {
+
+    protected double arm_length;
+    protected double leg_length;
+    protected double body_height;
+
+    public PersonDimensions(double arm_length, double leg_length, double body_height) {
+        this.arm_length = arm_length;
+        this.leg_length = leg_length;
+        this.body_height = body_height;
+    }
+
+    public double getArm_length() {
+        return arm_length;
+    }
+
+    public double getLeg_length() {
+        return leg_length;
+    }
+
+    public double getBody_height() {
+        return body_height;
+    }
 }
