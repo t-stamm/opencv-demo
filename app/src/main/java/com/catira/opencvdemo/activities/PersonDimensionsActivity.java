db4063cf8d05ef8d8b423b0b9ae9eb18d0619e25
ck <ck@infomantis.de>
2016-11-14 19:26:48 +0100

    add init personDimensions

---
+++ app/src/main/java/com/catira/opencvdemo/activities/PersonDimensionsActivity.java
@@ -0,0 +1,15 @@
+package com.catira.opencvdemo.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.catira.opencvdemo.R;
+
+public class PersonDimensionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_person_dimensions);
+    }
+}
