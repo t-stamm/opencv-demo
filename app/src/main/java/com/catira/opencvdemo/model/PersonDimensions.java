7919e803827ea09a2270028a8cc7d8a53fb8ab50
ck <ck@infomantis.de>
2016-12-03 22:17:45 +0100

    set minSDKVersion 21

--- app/src/main/java/com/catira/opencvdemo/model/PersonDimensions.java
+++ app/src/main/java/com/catira/opencvdemo/model/PersonDimensions.java
@@ -7,13 +7,15 @@ package com.catira.opencvdemo.model;
 public class PersonDimensions {
 
     protected double arm_length;
-    protected double leg_length;
+    protected double leg_length; //Schrittlänge
+    protected double snout_vent_length; //Rumpflänge
     protected double body_height;
 
-    public PersonDimensions(double arm_length, double leg_length, double body_height) {
+    public PersonDimensions(double arm_length, double leg_length, double body_height, double snout_vent_length) {
         this.arm_length = arm_length;
         this.leg_length = leg_length;
         this.body_height = body_height;
+        this.snout_vent_length = snout_vent_length;
     }
 
     public double getArm_length() {
