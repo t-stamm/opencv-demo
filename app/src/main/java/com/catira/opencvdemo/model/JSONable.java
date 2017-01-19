a1c178251692ab61b435c8844ed6582aada49df0
t-stamm <timo.stamm@gmx.de>
2017-01-19 22:22:17 +0100

    - Interface for getter to save json strings - service added to save preferences

---
+++ app/src/main/java/com/catira/opencvdemo/model/JSONable.java
@@ -0,0 +1,11 @@
+package com.catira.opencvdemo.model;
+
+import org.json.JSONObject;
+
+/**
+ * Created by timos on 19.01.2017.
+ */
+public interface JSONable {
+
+    JSONObject getJson();
+}
--- app/src/main/java/com/catira/opencvdemo/services/BikeCalculationHistoryStorage.java
+++ app/src/main/java/com/catira/opencvdemo/services/BikeCalculationHistoryStorage.java
@@ -1,8 +1,15 @@
 package com.catira.opencvdemo.services;
 
-import java.util.List;
+import android.content.Context;
+import android.content.SharedPreferences;
 
+import java.util.List;
+import java.util.concurrent.ThreadFactory;
+
+import com.catira.opencvdemo.R;
 import com.catira.opencvdemo.model.BikeCalculationHistoryEntry;
+
+import org.json.JSONArray;
 
 /**
  * Created by Timo on 26.10.2016.
@@ -10,10 +17,18 @@ import com.catira.opencvdemo.model.BikeCalculationHistoryEntry;
 
 public class BikeCalculationHistoryStorage {
 
+    private static String BIKE_HISTORY_SETTINGS = "bike_calculation_history";
+    private final SharedPreferences mSharedPref;
+
     private List<BikeCalculationHistoryEntry> entries;
+
+    public BikeCalculationHistoryStorage(Context context) {
+        this.mSharedPref = context.getSharedPreferences(BIKE_HISTORY_SETTINGS, Context.MODE_PRIVATE);
+    }
 
     public void addEntry(BikeCalculationHistoryEntry entry) {
         entries.add(entry);
+        save();
     }
 
     public List<BikeCalculationHistoryEntry> getEntries() {
@@ -21,6 +36,21 @@ public class BikeCalculationHistoryStorage {
     }
 
     public void save() {
-        // TODO save entries to android persistent storage
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                saveData();
+            }
+        }).run();
+    }
+
+    private void saveData() {
+        SharedPreferences.Editor editor = this.mSharedPref.edit();
+        JSONArray array = new JSONArray();
+        for(BikeCalculationHistoryEntry entry : entries) {
+            array.put(entry.getJson());
+        }
+        editor.putString(BIKE_HISTORY_SETTINGS, array.toString());
+        editor.commit();
     }
 }
