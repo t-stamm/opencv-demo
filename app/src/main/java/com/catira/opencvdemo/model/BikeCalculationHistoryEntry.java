4e34bb26f55bbb367b0b59d77faa4ea9566893d9
t-stamm <timo.stamm@gmx.de>
2017-01-19 22:20:18 +0100

    - changed properties according to Android Coding Guidelines - added json methods

--- app/src/main/java/com/catira/opencvdemo/model/BikeCalculationHistoryEntry.java
+++ app/src/main/java/com/catira/opencvdemo/model/BikeCalculationHistoryEntry.java
@@ -1,39 +1,66 @@
 package com.catira.opencvdemo.model;
 
-import android.media.Image;
+import android.graphics.Bitmap;
+import android.util.Base64;
+import android.util.Log;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
 import java.util.Calendar;
 
 /**
  * Created by Timo on 26.10.2016.
  */
 
-public class BikeCalculationHistoryEntry {
+public class BikeCalculationHistoryEntry implements JSONable {
 
-    private BikeDimensions capturedBike;
-    private PersonDimensions person;
-    private Calendar captured;
-    private Image bikeImage;
+    private BikeDimensions mCapturedBike;
+    private PersonDimensions mPerson;
+    private Calendar mCaptured;
+    private Bitmap mBikeImage;
 
-    public BikeCalculationHistoryEntry(BikeDimensions capturedBike, PersonDimensions person, Calendar captured, Image bikeImage) {
-        this.capturedBike = capturedBike;
-        this.person = person;
-        this.captured = captured;
-        this.bikeImage = bikeImage;
+    public BikeCalculationHistoryEntry(BikeDimensions capturedBike, PersonDimensions person, Calendar captured, Bitmap bikeImage) {
+        this.mCapturedBike = capturedBike;
+        this.mPerson = person;
+        this.mCaptured = captured;
+        this.mBikeImage = bikeImage;
     }
     public BikeDimensions getCapturedBike() {
-        return capturedBike;
+        return mCapturedBike;
     }
 
     public PersonDimensions getPerson() {
-        return person;
+        return mPerson;
     }
 
     public Calendar getCaptured() {
-        return captured;
+        return mCaptured;
     }
 
-    public Image getBikeImage() {
-        return bikeImage;
+    public Bitmap getBikeImage() {
+        return mBikeImage;
+    }
+
+    @Override
+    public JSONObject getJson() {
+        try {
+            JSONObject json = new JSONObject();
+            json.put("bikeDimensions", mCapturedBike.getJson());
+            json.put("personDimensions", mPerson.getJson());
+            json.put("mCaptured", mCaptured.toString());
+
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            mBikeImage.compress(Bitmap.CompressFormat.PNG, 100, baos);
+            byte[] b = baos.toByteArray();
+
+            json.put("image", Base64.encodeToString(b, Base64.DEFAULT));
+
+            return json;
+        } catch (JSONException e) {
+            Log.e(this.getClass().getName(), "Error converting object to json: "+e.getMessage());
+        }
+        return null;
     }
 }
