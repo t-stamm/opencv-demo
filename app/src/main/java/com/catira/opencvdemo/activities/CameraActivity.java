270094fdb309e19d7b6305f84b89766954a2b512
ck <ck@infomantis.de>
2017-01-28 21:09:14 +0100

    optimize set photo

--- app/src/main/java/com/catira/opencvdemo/activities/CameraActivity.java
+++ app/src/main/java/com/catira/opencvdemo/activities/CameraActivity.java
@@ -1,11 +1,19 @@
 package com.catira.opencvdemo.activities;
 
+import android.Manifest;
 import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
@@ -18,6 +26,9 @@ import android.widget.SeekBar;
 import com.catira.opencvdemo.R;
 import com.catira.opencvdemo.activities.components.MoveViewComponent;
 
+import java.io.File;
+import java.io.IOException;
+
 
 public class CameraActivity extends AppCompatActivity implements View.OnClickListener, View.OnTouchListener {
 
@@ -27,6 +38,7 @@ public class CameraActivity extends AppCompatActivity implements View.OnClickLis
     Intent i;
     final static int cameraData = 0;
     Bitmap bmp;
+    private File bildDatei;
 
     ImageButton weiter;
 
@@ -85,8 +97,11 @@ public class CameraActivity extends AppCompatActivity implements View.OnClickLis
         fa_button_camera = (ImageView) findViewById(R.id.fa_button_camera);
         fa_button_camera.setVisibility(View.GONE);
 
-        final Button resetButton = (Button) findViewById(R.id.buttonNextStep);
-        resetButton.setOnClickListener(new View.OnClickListener() {
+        Button buttonBacktoCycle = (Button) findViewById(R.id.buttonBacktoCycle);
+        buttonBacktoCycle.setVisibility(View.GONE);
+
+        final Button setPointsButton = (Button) findViewById(R.id.buttonNextStepSetPoints);
+        setPointsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 zview.updateMouse();
@@ -141,17 +156,79 @@ public class CameraActivity extends AppCompatActivity implements View.OnClickLis
     public void thirdStep(){
         seekBarWrapper.setVisibility(View.GONE);
         fa_button_check.setVisibility(View.VISIBLE);
+
+        final Button backToCycButton = (Button) findViewById(R.id.buttonBacktoCycle);
+        backToCycButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                zview.updateMouse();
+                secondStep();
+            }
+        });
+
+        backToCycButton.setVisibility(View.VISIBLE);
     }
     public void onClick(View v) {
+
+        int check = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if(check != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+            return;
+    }
+
+        cameraIntentSenden();
+
+        /*
         switch(v.getId()) {
             case R.id.fa_button_camera:
                 i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 startActivityForResult(i, cameraData);
                 break;
         }
+        */
+    }
+    private void cameraIntentSenden()  {
+        try {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            bildDatei = bildDateiErzeugen();
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(bildDatei));
+            startActivityForResult(intent, 1); // 1 = beliebige ID
+        } catch (IOException e) {
+            Log.d("FahrradApp", "Problem beim Intent-Senden", e);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) throws SecurityException{
+        if(grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            cameraIntentSenden();
+        }
+
+    }
+    private File bildDateiErzeugen() throws IOException {
+        String dateiName = "CameraSimple_" + System.currentTimeMillis() + ".jpg";
+
+        if(Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            File dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+            return new File(dir, dateiName);
+        }
+        else {
+            return null;
+        }
     }
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
         super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+            Bitmap bitmap = BitmapFactory.decodeFile(bildDatei.getAbsolutePath(), options);
+            bikecycleImageView.setImageBitmap(bitmap);
+            secondStep();
+        }
+        /*
         if(resultCode == RESULT_OK){
             Bundle extras = data.getExtras();
             bmp = (Bitmap) extras.get("data");
@@ -161,6 +238,7 @@ public class CameraActivity extends AppCompatActivity implements View.OnClickLis
             secondStep();
             //stateSetWheelCycle = true;
         }
+        */
     }
     @Override
     public boolean onTouch(View v, MotionEvent event) {
