d3186fa72171fd1f7874e2dc1532dde79f501467
ck <ck@infomantis.de>
2017-02-12 00:02:26 +0100

    add Listener on Btn :D

--- app/src/main/java/com/catira/opencvdemo/activities/ResultActivity.java
+++ app/src/main/java/com/catira/opencvdemo/activities/ResultActivity.java
@@ -3,14 +3,28 @@ package com.catira.opencvdemo.activities;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+
 import com.catira.opencvdemo.R;
+import com.catira.opencvdemo.services.BikeCalculationHistoryStorage;
 
 
-public class ResultActivity extends AppCompatActivity {
+public class ResultActivity extends AppCompatActivity implements Runnable {
+
+    ImageButton saveBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        saveBtn = (ImageButton) findViewById(R.id.fa_button_save);
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
     }
 }
